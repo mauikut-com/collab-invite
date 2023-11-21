f2fbdcc1416cf934c7530a3ef769924040a7d3dd
Reza <rezahandzalah@gmail.com>
2023-11-22 03:31:40 +0700

    use svelte

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  function parseLocation() {
+    const sp = new URLSearchParams(location.search);
+    // activity deal
+    return {
+      a: sp.get('a'),
+      d: sp.get('d') != 'n',
+    }
+  }
+  let params = parseLocation();
+
+  $: activity = params.a
+  $: deal = params.d
+
+  function fmt(activity, deal) {
+    return `?a=${activity}&d=${deal}`
+  }
+  $: history.replaceState({}, '', fmt(activity, deal));
+  
+</script>
+
+<main>
+  <input type="text" bind:value={activity} />
+
+  <label>deal:
+    <input
+      type="checkbox"
+      bind:checked={deal}
+      xxx="hide checkbox; style label to look like button" />
+  </label>
+
+</main>
+
+<style>
+</style>
